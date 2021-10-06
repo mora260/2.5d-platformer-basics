19fc0bdd898c59c2c9dc82b95cb8f4464c41c1a4
Andres Mora <mora1472@gmail.com>
2021-10-06 10:52:19 -0600

    Create a moving platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+
+  [SerializeField]
+  private Transform _pointA, _pointB;
+
+  [SerializeField]
+  private float _speed;
+
+  private bool _movingTowardsB;
+
+  private void Start() {
+    if (_pointA != null && _pointB != null)
+    _movingTowardsB = true;
+  }
+
+  private void FixedUpdate() {
+    if (_pointA != null && _pointB != null) {
+      if (_movingTowardsB) {
+        transform.position = Vector3.MoveTowards(transform.position, _pointB.transform.position, 0.01f * _speed);
+      } else {
+        transform.position = Vector3.MoveTowards(transform.position, _pointA.transform.position, 0.01f * _speed);
+      }
+
+      if(transform.position == _pointB.transform.position && _movingTowardsB) {
+        _movingTowardsB = false;
+      }
+
+      if(transform.position == _pointA.transform.position && !_movingTowardsB) {
+        _movingTowardsB = true;
+      }
+    }
+  }
+
   private void OnTriggerEnter(Collider other) {
     if (other.tag == "Player") {
       other.transform.parent = gameObject.transform;
@@ -17,7 +49,7 @@ public class Platform : MonoBehaviour
   }
 
   public void HitTheBottom (Collider other) {
-    if (other.gameObject.tag == "Player") {
+    if (other.tag == "Player") {
       Player player = GameObject.Find("Player").GetComponent<Player>();
 
       if ( player ) {
